a48bec42c6f1e63b91e7a6712621a761a4d3555c
aksr666 <aksr100793@mail.ru>
2019-02-24 13:35:56 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let crBase = base;
+  let primePows = [];
+  let prime = 2;
+  let obj = {prime: 0};
+  let arr= [];
+    while(crBase != 1){
+      if(crBase % prime == 0){
+        crBase /= prime;
+        primePows.push(prime);
+        if(!obj.hasOwnProperty(prime)){
+          obj[prime] = 0;
+        };
+        if(obj.hasOwnProperty(prime)){
+          obj[prime]++;
+        }
+      }
+      else{
+        prime++;
+      }
+    }
+    for (let i = 0; i < primePows.length; i++) {
+      let pow = primePows[i];
+      let exp = primePows[i];
+      let tempResult = 0;
+        while(number > pow){
+        tempResult += Math.floor(number / pow);
+        pow *= exp;
+      }
+      arr.push(Math.floor(tempResult / obj[exp]));     
+    }
+    let result = Math.min.apply('', arr);
+    return result;
 }
